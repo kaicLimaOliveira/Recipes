91743076d3f8095c1347e98eee2997cd03c2026a
kaicLimaOliveira <kaic10@outlook.com>
2022-04-19 16:13:39 -0300

    Registrando os models no admin

--- apps/recipes/models.py
+++ apps/recipes/models.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-
 class Category(models.Model):
     name = models.CharField(max_length=65)
+    
+    def __str__(self):
+        return self.name
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
@@ -21,4 +23,7 @@ class Recipe(models.Model):
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    
+    def __str__(self):
+        return self.title
     
